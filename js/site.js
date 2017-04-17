cf7a650aef9024d99cf1a8410045850c285e1660
ddelvadi <ddelvadi@hawk.iit.edu>
2017-04-17 12:41:48 -0500

    Added password value 'user'

--- js/site.js
+++ js/site.js
@@ -4,8 +4,8 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
     
-  $('#IITLogin').on('submit-button', function(e){
-    if ($('#name-field').val() === 'user') {
+  $('#IITLogin').on('submit', function(e){
+    if ($('#name-field').val() === 'user' && $('#password-field').val() === 'user') {
           window.location.replace('finances/index.html');
       } else {
           console.log('Bad username');

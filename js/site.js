1708c1f7286b4eccd879702053b9bd3fa2063670
Jabran Shakil <jabranshakil@live.com>
2017-04-16 22:29:01 -0500

    Hardcoded the login credentials

--- js/site.js
+++ js/site.js
@@ -1,24 +1,16 @@
-jQuery(document).ready(function() {
-  function close_content_section() {
-	jQuery('.content .content-section-title').removeClass('active');
-	jQuery('.content .content-section-content').slideUp(300).removeClass('open');
-  }
+$.noConflict();
 
-  jQuery('.content-section-title').click(function(e) {
-		// Grab current anchor value
-	var currentAttrValue = jQuery(this).attr('href');
-
-	if(jQuery(e.target).is('.active')) {
-	  close_content_section();
-	}else {
-	  close_content_section();
-
-	  // Add active class to section title
-	  jQuery(this).addClass('active');
-	  // Open up the hidden content panel
-	  jQuery('.content ' + currentAttrValue).slideDown(300).addClass('open'); 
-	}
-
-	e.preventDefault();
-  });
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+    
+  $('#IITLogin').on('submit-button', function(e){
+    if ($('#name-field').val() === 'user') {
+          window.location.replace('finances/index.html');
+      } else {
+          console.log('Bad username');
+          $('#IITLogin').prepend('<div class="incorrect-credentials">Invalid credentials</div>');
+      }
+      e.preventDefault();           
+  })   
 });
